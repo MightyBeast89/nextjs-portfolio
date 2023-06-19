d026dc956d0ed39fdf8e3646c3f02bd4b0fa17ba
MightyBeast <variola@my.centennialcollege.ca>
2023-06-19 09:34:26 -0400

    initial commit

---
+++ src/pages/_app.js
@@ -0,0 +1,24 @@
+import Navbar from "@/components/Navbar";
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+
+const monserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-iner",
+});
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <header>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <main
+          className={`${monserrat.className} font-mont bg-light w-full min-h-screen`}
+        >
+          <Navbar />
+          <Component {...pageProps} />;
+        </main>
+      </header>
+    </>
+  );
+}
