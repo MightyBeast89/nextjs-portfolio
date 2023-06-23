60ad8c42375a2ad17577b40cdc8f0446b9c2631d
MightyBeast <variola@my.centennialcollege.ca>
2023-06-22 22:15:21 -0400

    add homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,73 @@
 import React from "react";
 import Link from "next/link";
 import Logo from "./Logo";
+import { useRouter } from "next/router";
+import {
+  FacebookIcon,
+  GithubIcon,
+  LinkedInIcon,
+  TwitterIcon,
+} from "./Icons.jsx";
+import { motion } from "framer-motion";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`h-[1px] inline-block w-0 bg-dark absolute left-0 bottom-0.5 group-hover:w-fulll transition-[width] ease duration-300} ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
 
 const Navbar = () => {
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/newsletter">Newsletter</Link>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
+        <CustomLink href="/newsletter" title="News Letter" className="ml-4" />
       </nav>
-      <h2>
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a
+          href="https://github.com/MightyBeast89"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mr-3"
+        >
+          <GithubIcon />
+        </motion.a>
+        <motion.a
+          href="/"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a
+          href="/"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 ml-3"
+        >
+          <FacebookIcon />
+        </motion.a>
+      </nav>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
-      </h2>
-
-      <nav>
-        <Link href="/" target={"_blank"}>
-          Github
-        </Link>
-        <Link href="/" target={"_blank"}>
-          Linkedin
-        </Link>
-        <Link href="/" target={"_blank"}>
-          Facebook
-        </Link>
-      </nav>
+      </div>
     </header>
   );
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import { Montserrat } from "next/font/google";
@@ -16,7 +17,8 @@ export default function App({ Component, pageProps }) {
           className={`${monserrat.className} font-mont bg-light w-full min-h-screen`}
         >
           <Navbar />
-          <Component {...pageProps} />;
+          <Component {...pageProps} />
+          <Footer />
         </main>
       </header>
     </>
