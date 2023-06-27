8a591c9d156aca5084cb81ead9fb4000d1bb5da1
MightyBeast <variola@my.centennialcollege.ca>
2023-06-26 22:30:34 -0400

    update project component

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,21 +9,38 @@ import project1 from "../../public/images/projects/project-1.jpeg";
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
-    <article>
-      <Link href={link} target="_blank">
+    <article
+      className="w-full flex items-center justify-between relative rounded-br-2xl
+    rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12"
+    >
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl" />
+
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+      >
         <Image src={img} alt={title} className="w-full h-auto " />
       </Link>
-      <div>
-        <span>{type}</span>
-        <Link href={link} target="_blank">
-          <h2>{title}</h2>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
         </Link>
-        <p>{summary}</p>
-        <div>
-          <Link href={github} target="_blank">
+        <p className="my-2 font-medium text-dark">{summary}</p>
+        <div className="mt-2 flex items-center">
+          <Link href={github} target="_blank" className="w-10">
             <GithubIcon />
           </Link>
-          <Link href={link} target="_blank">
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+          >
             Visit Project
           </Link>
         </div>
@@ -31,6 +48,47 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
     </article>
   );
 };
+
+const Project = ({ title, type, img, link, github }) => {
+  return (
+    <article
+      className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid
+    border-dark bg-light p-6 relative"
+    >
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl" />
+      <Link
+        href={link}
+        target="_blank"
+        className="w-full cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto " />
+      </Link>
+      <div className="w-full flex flex-col items-start justify-between mt-4">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
+        </Link>
+        <div className="w-full mt-2 flex items-center justify-between">
+          <Link
+            href={link}
+            target="_blank"
+            className="text-lg font-semibold underline"
+          >
+            Visit
+          </Link>
+          <Link href={github} target="_blank" className="w-8">
+            <GithubIcon />
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
 const projects = () => {
   return (
     <>
@@ -40,23 +98,65 @@ const projects = () => {
       </Head>
       <main className="w-full mb-16 flex flex-col items-center justify-center">
         <Layout className="pt-16">
-          <AnimatedText text="Dedication Leads to Success" />
-          <div className="grid grid-cols-12 gap-24">
+          <AnimatedText text="Dedication Leads to Success" className="mb-16" />
+
+          <div className="grid grid-cols-12 gap-24 gap-y-32">
             <div className="col-span-12">
               <FeaturedProject
                 title="Animal Shelter"
                 img={project1}
-                summary="description"
+                summary="This is a MERN social media application develop to connect people."
                 link="/"
                 github="/"
                 type="Featured Project"
               />
             </div>
-            <div className="col-span-6">Project 1</div>
-            <div className="col-span-6">Project 2</div>
-            <div className="col-span-12">Featured Project</div>
-            <div className="col-span-6">Project 3</div>
-            <div className="col-span-6">Project 4</div>
+            <div className="col-span-6">
+              <Project
+                title="Animal Shelter"
+                img={project1}
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Animal Shelter"
+                img={project1}
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Animal Shelter"
+                img={project1}
+                summary="This is a MERN social media application develop to connect people."
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Animal Shelter"
+                img={project1}
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Animal Shelter"
+                img={project1}
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
           </div>
         </Layout>
       </main>
