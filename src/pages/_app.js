2643c40062df121c7aaa79606f4b51925839ff02
MightyBeast <variola@my.centennialcollege.ca>
2023-07-04 15:34:21 -0400

    updated all components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <main
-          className={`${monserrat.className} font-mont bg-light w-full min-h-screen`}
+          className={`${monserrat.className} font-mont bg-light dark:bg-dark w-full min-h-screen`}
         >
           <Navbar />
           <Component {...pageProps} />
--- src/pages/about.js
+++ src/pages/about.js
@@ -39,12 +39,12 @@ const about = () => {
         <title>Vince Ariola | About Page</title>
         <meta name="description" content=" about page" />
       </Head>
-      <main className="flex w-full flex-col items-center justify-center ">
+      <main className="flex w-full flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
           <AnimatedText text="Passion fuels purpose" className="mb-20" />
           <div className="grid w-full grid-cols-8 gap-16">
             <div className="col-span-3 flex flex-col items-start justify-start">
-              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
                 About me
               </h2>
               <p className="my-4 font-medium">
@@ -53,12 +53,16 @@ const about = () => {
               </p>
             </div>
 
-            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
-              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light">
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
               <Image
                 src={profilePic}
                 alt="Vince Ariola"
                 className="w-full h-auto rounded-2xl"
+                priority
+                sizes="(max-width:768px)100vw,
+                (max-width:1200px)50vw,
+                33vw"
               />
             </div>
             <div className="col-span-2 flex flex-col items-end justify-between">
@@ -66,7 +70,7 @@ const about = () => {
                 <span className="inline-block text-7xl font-bold">
                   <AnimatedNumbers value={10} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75">
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">
                   Satisfied Clients
                 </h2>
               </div>
@@ -74,7 +78,7 @@ const about = () => {
                 <span className="inline-block text-7xl font-bold">
                   <AnimatedNumbers value={15} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75">
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">
                   Projects Completed
                 </h2>
               </div>
@@ -82,7 +86,7 @@ const about = () => {
                 <span className="inline-block text-7xl font-bold">
                   <AnimatedNumbers value={1} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75">
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">
                   years of experience
                 </h2>
               </div>
