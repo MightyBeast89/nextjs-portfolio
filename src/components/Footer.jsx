3733b525ac6861f563e62b3561a145ee5832e62c
MightyBeast <variola@my.centennialcollege.ca>
2023-07-04 21:29:13 -0400

    mobile responsive update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
   return (
     <footer
       className="w-full border-t-2 border-solid
-     border-dark font-medium text-lg dark:text-light dark:border-light"
+     border-dark font-medium text-lg dark:text-light dark:border-light sm:text-base"
     >
-      <Layout className="py-8 flex items-center justify-between">
+      <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
         <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
         <Link href="/" className="underline underline-offset-2">
           Code by Vince Ariola
--- src/pages/articles.js
+++ src/pages/articles.js
@@ -8,6 +8,7 @@ import article1 from "../../public/images/articles/create loading screen in reac
 import article2 from "../../public/images/articles/form validation in reactjs using custom react hook.png";
 import { motion, useMotionValue } from "framer-motion";
 import article3 from "../../public/images/articles/form validation in reactjs using custom react hook.png";
+import TransitionEffect from "@/components/TransitionEffect";
 
 const FramerImage = motion(Image);
 
@@ -48,7 +49,7 @@ const MovingImage = ({ title, img, link }) => {
         sizes="(max-width:768px)100vw,
                 (max-width:1200px)50vw,
                 50vw"
-        className="z-10 w-96 h-auto hidden absolute rounded-lg"
+        className="z-10 w-96 h-auto hidden absolute rounded-lg md:!hidden"
       />
     </Link>
   );
@@ -61,10 +62,14 @@ const Article = ({ img, title, date, link }) => {
       whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeOut" } }}
       viewport={{ once: true }}
       className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid
-     border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light"
+     border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light
+     sm:flex-col"
     >
       <MovingImage title={title} img={img} link={link} />
-      <span className="text-primary font-semibold pl-4 dark:text-primaryDark">
+      <span
+        className="text-primary font-semibold pl-4 dark:text-primaryDark sm:self-start
+      sm:pl-0 xs:text-sm"
+      >
         {date}
       </span>
     </motion.li>
@@ -86,10 +91,14 @@ const FeaturedArticles = ({ img, title, time, summary, link }) => {
           className="w-full h-auto "
           whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.2 }}
+          priority
+          sizes="(max-width:768px)100vw,
+                (max-width:1200px)50vw,
+                50vw,"
         />
       </Link>
       <Link href={link} target="_blank">
-        <h2 className="capitalize text-2xl font-bold mt-4 my-2 hover:underline ">
+        <h2 className="capitalize text-2xl font-bold mt-4 my-2 hover:underline xs:text-lg">
           {title}
         </h2>
       </Link>
@@ -105,16 +114,17 @@ const articles = () => {
   return (
     <>
       <Head>
-        <title>Vince Ariola | Testimonials Page</title>
-        <meta name="description" content=" testimonials page" />
+        <title>Vince Ariola | Article Page</title>
+        <meta name="description" content=" article page" />
       </Head>
+      <TransitionEffect />
       <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light">
         <Layout className="pt-16">
           <AnimatedText
             text="Feedback will help me improve"
-            className="mb-16"
+            className="mb-16 lg:!text-7xl sm:!mb-8 sm:!text-6xl xs:!text-4xl"
           />
-          <ul className="grid grid-cols-2 gap-16">
+          <ul className="grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16">
             <FeaturedArticles
               img={article1}
               title="Article 1 "
